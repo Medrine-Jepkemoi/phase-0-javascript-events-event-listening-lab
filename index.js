e7f2a77264c8c0d60d66bfc4fafa0b72944731f5
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-03-25 08:15:46 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function addingEventListener() {
 
-    const input = document.getElementById('button');
-input.addEventListener('click', function() {
+  const input = document.getElementById("button");
+
+function clickAlert() {
   alert('I was clicked!');
-});
 }
+
+input.addEventListener('click', clickAlert);
+}
+
+addingEventListener()
